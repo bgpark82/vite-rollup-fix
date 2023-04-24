3a1a3ed530846e81641da99a188dbb10f6154812
woo.choi <woo.choi@musinsa.com>
2023-04-24 17:23:04 +0900

    chore: stat 서브 프로젝트 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,2 @@
 rootProject.name = "dataplatform"
+include("stat")
---
+++ stat/src/test/kotlin/com/musinsa/stat/StatApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.musinsa.stat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StatApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
