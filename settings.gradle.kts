38459966d496cf476871e8a9ea560a7345492e0f
woo.choi <woo.choi@musinsa.com>
2023-04-25 16:53:41 +0900

    chore: pluginManagement 적용

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,15 @@
 rootProject.name = "dataplatform"
 include("stat")
+
+pluginManagement {
+    val springBootVersion: String by settings
+    val springBootDependencyManagementVersion: String by settings
+    val kotlinVersion: String by settings
+
+    plugins {
+        id("org.springframework.boot") version springBootVersion
+        id("io.spring.dependency-management") version springBootDependencyManagementVersion
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.spring") version kotlinVersion
+    }
+}
--- stat/build.gradle.kts
+++ stat/build.gradle.kts
@@ -1,15 +1,24 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.6"
-	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.7.22"
-	kotlin("plugin.spring") version "1.7.22"
+	// 스프링부트
+	id("org.springframework.boot")
+
+	// 스프링부트 버전에 맞는 의존성을 가져오도록 도와주는 플러그인
+	id("io.spring.dependency-management")
+
+	// Kotlin JVM plugin
+	kotlin("jvm")
+
+	// 특정한 어노테이션이 붙은 클래스의 접근 제한자를 open 으로 모두 변경(https://kotlinlang.org/docs/all-open-plugin.html#spring-support)
+	kotlin("plugin.spring")
 }
 
 group = "com.musinsa"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
+
+val javaVersion: String by project
 
 repositories {
 	mavenCentral()
@@ -17,14 +26,17 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	// 코틀린 리플렉션
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
 }
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
+		// 어노테이션을 활용해 결함 탐지. Java 표준에 반영되지 않았음. @NonNull, @CheckForNull 등을 정의
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
+		jvmTarget = javaVersion
 	}
 }
 
