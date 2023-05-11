7ae69a9b896d102f0004c946acf4c3fc2cb957cc
woo.choi <woo.choi@musinsa.com>
2023-05-11 16:19:13 +0900

    feat: 매출통계 유형 추가

---
+++ stat/src/main/kotlin/com/musinsa/stat/sales/domain/Metric.kt
@@ -0,0 +1,16 @@
+package com.musinsa.stat.sales.domain
+
+/**
+ * 매출 통계 유형
+ */
+enum class Metric(val description: String) {
+    DAILY("일별"),
+    MONTLY("월별"),
+    PARTNER("업체별"),
+    BRAND("브랜드별"),
+    BRAND_PARTNER("브랜드업체별"),
+    GOODS("상품별"),
+    AD("광고별"),
+    COUPON("쿠폰별"),
+    CATEGORY("카테고리별"),
+}
