3deb1910d30578607569b23da75ab40926142384
woo.choi <woo.choi@musinsa.com>
2023-05-11 14:51:40 +0900

    feat: 매출통계 쿼리 저장소 추가

--- stat/src/main/kotlin/com/musinsa/stat/sales/config/QueryStore.kt
+++ stat/src/main/kotlin/com/musinsa/stat/sales/config/QueryStore.kt
@@ -2,9 +2,18 @@ package com.musinsa.stat.sales.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-
 /**
  * 브릭레인 Query Id 저장소
  */
 @ConfigurationProperties(prefix = "custom.databricks.stats-query-id")
-data class QueryStore(val daily: String)
+data class QueryStore(
+    val daily: String,
+    val montly: String,
+    val partner: String,
+    val brand: String,
+    val brandPartner: String,
+    val goods: String,
+    val ad: String,
+    val coupon: String,
+    val category: String
+)
