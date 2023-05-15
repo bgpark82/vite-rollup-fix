5d1289601b55c1c2e26f606222b920a9c1a1e076
woo.choi <woo.choi@musinsa.com>
2023-05-15 13:54:11 +0900

    docs: NoArgsConstructor 사용방법 자세히 추가

--- stat/src/main/kotlin/com/musinsa/stat/util/NoArgsConstructor.kt
+++ stat/src/main/kotlin/com/musinsa/stat/util/NoArgsConstructor.kt
@@ -2,6 +2,7 @@ package com.musinsa.stat.util
 
 /**
  * 빈 생성자를 만들기 위한 어노테이션
+ * 프라이머리 생성자가 없는 클래스의 생성자가 2개 이상일 경우, 아래 어노테이션을 추가한다.
  */
 @Target(AnnotationTarget.CLASS)
 annotation class NoArgsConstructor
