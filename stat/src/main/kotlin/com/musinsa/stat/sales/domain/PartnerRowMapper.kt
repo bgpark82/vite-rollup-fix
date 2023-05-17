a3ee1e0886d5f19f846f1668a1bc84c7fd7ce214
woo.choi <woo.choi@musinsa.com>
2023-05-17 10:11:00 +0900

    feat: 업체별 RowMapper 추가

--- stat/src/main/kotlin/com/musinsa/stat/sales/domain/PartnerRowMapper.kt
+++ stat/src/main/kotlin/com/musinsa/stat/sales/domain/PartnerRowMapper.kt
@@ -1,3 +1,19 @@
 package com.musinsa.stat.sales.domain
 
-object PartnerRowMapper
+import com.musinsa.stat.sales.dto.Partner
+import org.springframework.jdbc.core.RowMapper
+import java.sql.ResultSet
+
+/**
+ * 업체별 RowMapper
+ */
+object PartnerRowMapper : RowMapper<Partner> {
+    override fun mapRow(rs: ResultSet, rowNum: Int): Partner {
+        return Partner(
+            rs,
+            partnerId = rs.getString("업체코드"),
+            partnerName = rs.getString("업체명"),
+            partnerType = rs.getString("업체구분")
+        )
+    }
+}
