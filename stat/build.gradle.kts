2f75a82c93acfecf6c8955c915891da6d4e32fc5
woo.choi <woo.choi@musinsa.com>
2023-05-22 14:17:20 +0900

    refactor: build script 정리

--- stat/build.gradle.kts
+++ stat/build.gradle.kts
@@ -38,40 +38,6 @@ val MAIN_CLASS = "com.musinsa.stat.StatApplication"
 @Suppress("PropertyName")
 val SNIPPETS_DIR by extra { file("build/generated-snippets") }
 
-// Ascii Doc Create Tasks
-tasks {
-    // Test 결과를 snippet Directory
-    test {
-        outputs.dir(SNIPPETS_DIR)
-    }
-
-    asciidoctor {
-        // test 가 성공해야만, 아래 Task 실행
-        dependsOn(test)
-
-        // 기존에 존재하는 Docs 삭제
-        doFirst {
-            delete(file("src/main/resources/static/docs"))
-        }
-
-        // Directory 설정
-        inputs.dir(SNIPPETS_DIR)
-
-        // Ascii Doc 파일 복사
-        doLast {
-            copy {
-                from("build/docs/asciidoc")
-                into("src/main/resources/static/docs")
-            }
-        }
-    }
-
-    build {
-        // Ascii Doc 파일 생성이 성공해야만, Build 진행
-        dependsOn(asciidoctor)
-    }
-}
-
 application {
     mainClass.set(MAIN_CLASS)
 }
@@ -118,30 +84,64 @@ dependencies {
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        // 어노테이션을 활용해 결함 탐지. Java 표준에 반영되지 않았음. @NonNull, @CheckForNull 등을 정의
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = JAVA_VERSION
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            // 어노테이션을 활용해 결함 탐지. Java 표준에 반영되지 않았음. @NonNull, @CheckForNull 등을 정의
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = JAVA_VERSION
+        }
     }
-}
 
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
+    jar {
+        // Plain jar 생성 금지
+        enabled = false
+    }
 
-tasks.jar {
-    // Plain jar 생성 금지
-    enabled = false
-}
-
-tasks.bootJar {
-    // jar 파일명 지정
-    archiveFileName.set("stat.jar")
+    bootJar {
+        // jar 파일명 지정
+        archiveFileName.set("stat.jar")
+    }
 }
 
 // Gradle Bug 로 인해 Main Class 를 별도로 지정(Gradle 에서 MainClass 를 kt로 지정 중)
 // @see https://stackoverflow.com/questions/73438308/aws-beanstalk-kotlin-spring-boot-nosuchmethodexception-main
 tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
     mainClass.set(StringBuilder(MAIN_CLASS).append("Kt").toString())
+}
+
+
+// Ascii Doc Create Tasks
+tasks {
+    // Test 결과를 snippet Directory
+    test {
+        outputs.dir(SNIPPETS_DIR)
+        useJUnitPlatform()
+    }
+
+    asciidoctor {
+        // test 가 성공해야만, 아래 Task 실행
+        dependsOn(test)
+
+        // 기존에 존재하는 Docs 삭제
+        doFirst {
+            delete(file("src/main/resources/static/docs"))
+        }
+
+        // Directory 설정
+        inputs.dir(SNIPPETS_DIR)
+
+        // Ascii Doc 파일 복사
+        doLast {
+            copy {
+                from("build/docs/asciidoc")
+                into("src/main/resources/static/docs")
+            }
+        }
+    }
+
+    build {
+        // Ascii Doc 파일 생성이 성공해야만, Build 진행
+        dependsOn(asciidoctor)
+    }
 }
