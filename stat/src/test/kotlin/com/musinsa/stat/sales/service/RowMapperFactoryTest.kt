8df9ec984f6184492cb65a8076040652fe541ff4
woo.choi <woo.choi@musinsa.com>
2023-05-22 10:11:21 +0900

    test: RowMapperFactory 테스트 코드 완성

--- stat/src/test/kotlin/com/musinsa/stat/sales/service/RowMapperFactoryTest.kt
+++ stat/src/test/kotlin/com/musinsa/stat/sales/service/RowMapperFactoryTest.kt
@@ -1,62 +1,72 @@
 package com.musinsa.stat.sales.service
 
-import com.musinsa.stat.sales.domain.DailyAndMontlyRowMapper
-import com.musinsa.stat.sales.domain.Metric
-import com.musinsa.stat.sales.domain.PartnerRowMapper
+import com.musinsa.stat.sales.domain.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 private class RowMapperFactoryTest {
 
     @Test
-    fun 일별매출통계_ROW_MAPPER를_가져온다() {
+    fun 일별매출통계_ROW_MAPPER_를_가져온다() {
         assertThat(RowMapperFactory.getRowMapper(Metric.DAILY)).isInstanceOf(
             DailyAndMontlyRowMapper::class.java
         )
     }
 
     @Test
-    fun 월별매출통계_ROW_MAPPER를_가져온다() {
+    fun 월별매출통계_ROW_MAPPER_를_가져온다() {
         assertThat(RowMapperFactory.getRowMapper(Metric.MONTLY)).isInstanceOf(
             DailyAndMontlyRowMapper::class.java
         )
     }
 
     @Test
-    fun 업체별매출통계_ROW_MAPPER를_가져온다() {
+    fun 업체별매출통계_ROW_MAPPER_를_가져온다() {
         assertThat(RowMapperFactory.getRowMapper(Metric.PARTNER)).isInstanceOf(
             PartnerRowMapper::class.java
         )
     }
 
     @Test
-    fun 브랜드별매출통계_ROW_MAPPER를_가져온다() {
-        // TODO
+    fun 브랜드별매출통계_ROW_MAPPER_를_가져온다() {
+        assertThat(RowMapperFactory.getRowMapper(Metric.BRAND)).isInstanceOf(
+            BrandRowMapper::class.java
+        )
     }
 
     @Test
-    fun 브랜드업체별매출통계_ROW_MAPPER를_가져온다() {
-        // TODO
+    fun 브랜드업체별매출통계_ROW_MAPPER_를_가져온다() {
+        assertThat(RowMapperFactory.getRowMapper(Metric.BRAND_PARTNER)).isInstanceOf(
+            BrandPartnerRowMapper::class.java
+        )
     }
 
     @Test
-    fun 상품별매출통계_ROW_MAPPER를_가져온다() {
-        // TODO
+    fun 상품별매출통계_ROW_MAPPER_를_가져온다() {
+        assertThat(RowMapperFactory.getRowMapper(Metric.GOODS)).isInstanceOf(
+            GoodsRowMapper::class.java
+        )
     }
 
     @Test
-    fun 광고별매출통계_ROW_MAPPER를_가져온다() {
-        // TODO
+    fun 광고별매출통계_ROW_MAPPER_를_가져온다() {
+        assertThat(RowMapperFactory.getRowMapper(Metric.AD)).isInstanceOf(
+            AdRowMapper::class.java
+        )
     }
 
     @Test
-    fun 쿠폰별매출통계_ROW_MAPPER를_가져온다() {
-        // TODO
+    fun 쿠폰별매출통계_ROW_MAPPER_를_가져온다() {
+        assertThat(RowMapperFactory.getRowMapper(Metric.COUPON)).isInstanceOf(
+            CouponRowMapper::class.java
+        )
     }
 
     @Test
-    fun 카테고리별매출통계_ROW_MAPPER를_가져온다() {
-        // TODO
+    fun 카테고리별매출통계_ROW_MAPPER_를_가져온다() {
+        assertThat(RowMapperFactory.getRowMapper(Metric.CATEGORY)).isInstanceOf(
+            CategoryRowMapper::class.java
+        )
     }
 
 }
