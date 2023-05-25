b50511ad717a44b16e8b229159bf992c089a6cc2
woo.choi <woo.choi@musinsa.com>
2023-05-25 11:52:37 +0900

    feat: 브랜드 클래스 추가

---
+++ stat/src/main/kotlin/com/musinsa/stat/brand/dto/Brand.kt
@@ -0,0 +1,33 @@
+package com.musinsa.stat.brand.dto
+
+data class Brand(
+    /**
+     * 브랜드분류(S:시스템,U:사용자,G:해외전용)
+     */
+    val brandType: String,
+
+    /**
+     * 브랜드
+     */
+    val brandId: String,
+
+    /**
+     * 브랜드명
+     */
+    val brandName: String,
+
+    /**
+     * 브랜드명_영문
+     */
+    val brandNameEn: String,
+
+    /**
+     * 사용여부(Y/N)
+     */
+    val used: Boolean,
+
+    /**
+     * 글로벌 여부(True/False)
+     */
+    val isGlobal: Boolean
+)
