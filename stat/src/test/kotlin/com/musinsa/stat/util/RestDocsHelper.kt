33d46d2b74acc59096dc9cf6b290d997e2f7c857
woo.choi <woo.choi@musinsa.com>
2023-06-05 13:01:20 +0900

    docs: host, scheme, HTTPS 호출 명시

--- stat/src/test/kotlin/com/musinsa/stat/util/RestDocsHelper.kt
+++ stat/src/test/kotlin/com/musinsa/stat/util/RestDocsHelper.kt
@@ -27,9 +27,9 @@ import org.springframework.web.filter.CharacterEncodingFilter
  * Spring REST Docs 문서 생성에 필요한 기능들
  */
 
-// TODO HOST, PORT 수정
-private const val HOST = "3.35.207.130"
-private const val PORT = 8080
+private const val SCHEME = "https"
+private const val HOST = "stat.data.musinsa.com"
+private const val PORT = 443
 private const val UTF8 = "UTF-8"
 
 /**
@@ -65,6 +65,7 @@ fun buildMockMvc(
 
     // Host, port 설정
     mockMvcRestDocumentationConfigurer.uris().withHost(HOST).withPort(PORT)
+        .withScheme(SCHEME)
 
     return MockMvcBuilders.webAppContextSetup(webApplicationContext)
         .addFilter<DefaultMockMvcBuilder>(CharacterEncodingFilter(UTF8, true))
