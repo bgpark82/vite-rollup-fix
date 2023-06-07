532f13edd1aeac0fb3c14f3555b49d7bdffa597a
woo.choi <woo.choi@musinsa.com>
2023-06-07 14:39:24 +0900

    test: Metric Enum 컨트롤러 추가

---
+++ stat/src/test/kotlin/com/musinsa/stat/sales/domain/MetricController.kt
@@ -0,0 +1,18 @@
+package com.musinsa.stat.sales.domain
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Metric Enum 아스키독 노출을 위한 테스트 전용 컨트롤러
+ */
+@RestController
+@RequestMapping("/test/metric")
+internal class MetricController {
+
+    @GetMapping
+    fun getMetrics(): Map<String, String> {
+        return Metric.values().associate { Pair(it.name, it.description) }
+    }
+}
