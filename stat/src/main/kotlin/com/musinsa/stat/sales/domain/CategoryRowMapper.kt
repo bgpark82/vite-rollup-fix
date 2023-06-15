9221aa7979c1f78802f0bbdd3b997d8b46c15495
woo.choi <woo.choi@musinsa.com>
2023-06-15 11:23:06 +0900

    feat: 카테고리 nullable 추가

--- stat/src/main/kotlin/com/musinsa/stat/sales/domain/CategoryRowMapper.kt
+++ stat/src/main/kotlin/com/musinsa/stat/sales/domain/CategoryRowMapper.kt
@@ -5,13 +5,14 @@ import org.springframework.jdbc.core.RowMapper
 import java.sql.ResultSet
 
 object CategoryRowMapper : RowMapper<Category> {
+    private const val NULL_VALUE = "값없음"
     override fun mapRow(rs: ResultSet, rowNum: Int): Category {
         return Category(
             rs,
-            largeCategoryCode = rs.getString("대분류") ?: null,
-            mediumCategoryCode = rs.getString("중분류") ?: null,
-            smallCategoryCode = rs.getString("소분류") ?: null,
-            category = rs.getString("카테고리명") ?: null
+            largeCategoryCode = rs.getString("대분류") ?: NULL_VALUE,
+            mediumCategoryCode = rs.getString("중분류") ?: NULL_VALUE,
+            smallCategoryCode = rs.getString("소분류") ?: NULL_VALUE,
+            category = rs.getString("카테고리명") ?: NULL_VALUE
         )
     }
 }
--- stat/src/main/kotlin/com/musinsa/stat/sales/domain/GoodsRowMapper.kt
+++ stat/src/main/kotlin/com/musinsa/stat/sales/domain/GoodsRowMapper.kt
@@ -5,6 +5,8 @@ import org.springframework.jdbc.core.RowMapper
 import java.sql.ResultSet
 
 object GoodsRowMapper : RowMapper<Goods> {
+    private const val NULL_VALUE = "값없음"
+
     override fun mapRow(rs: ResultSet, rowNum: Int): Goods {
         return Goods(
             rs,
@@ -12,7 +14,7 @@ object GoodsRowMapper : RowMapper<Goods> {
             goodsName = rs.getString("상품명"),
             styleNumber = rs.getString("스타일넘버"),
             brandName = rs.getString("브랜드명"),
-            category = rs.getString("카테고리"),
+            category = rs.getString("카테고리") ?: NULL_VALUE,
             mdId = rs.getString("담당MD"),
             goodsStatusName = rs.getString("상품상태")
         )
