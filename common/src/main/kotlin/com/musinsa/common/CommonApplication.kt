695d24b05cef6d6615f984d1f55cfa9c570a3138
woo.choi <woo.choi@musinsa.com>
2023-06-22 11:05:13 +0900

    chore: common 플러그인, 의존성 추가

--- common/src/main/kotlin/com/musinsa/common/CommonApplication.kt
+++ common/src/main/kotlin/com/musinsa/common/CommonApplication.kt
@@ -1,7 +1,9 @@
 package com.musinsa.common
 
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@SpringBootApplication
 class CommonApplication
 
 fun main(args: Array<String>) {
