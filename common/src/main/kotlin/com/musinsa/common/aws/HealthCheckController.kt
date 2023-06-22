4387bfdd9c2b2e6666d55bf01be561c816de8aae
woo.choi <woo.choi@musinsa.com>
2023-06-22 11:38:02 +0900

    refactor: 헬스체크 컨트롤러 이동

--- common/src/main/kotlin/com/musinsa/common/aws/HealthCheckController.kt
+++ common/src/main/kotlin/com/musinsa/common/aws/HealthCheckController.kt
@@ -1,4 +1,4 @@
-package com.musinsa.stat.aws
+package com.musinsa.common.aws
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
--- stat/src/main/kotlin/com/musinsa/stat/StatApplication.kt
+++ stat/src/main/kotlin/com/musinsa/stat/StatApplication.kt
@@ -1,5 +1,6 @@
 package com.musinsa.stat
 
+import com.musinsa.common.aws.HealthCheckController
 import com.musinsa.common.config.CorsConfig
 import com.musinsa.stat.databricks.config.DatabricksHttpConnectionConfig
 import com.musinsa.stat.sales.config.QueryStore
@@ -14,7 +15,7 @@ import org.springframework.context.annotation.Import
 // @ConstructorBinding 사용시 필수
 @EnableConfigurationProperties(value = [DatabricksHttpConnectionConfig::class, QueryStore::class, SearchQueryStore::class])
 
-@Import(value = [CorsConfig::class])
+@Import(value = [CorsConfig::class, HealthCheckController::class])
 class StatApplication
 
 fun main(args: Array<String>) {
