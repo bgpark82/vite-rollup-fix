4cdb7ee69924735d7d2856b1abbb7b832ed157e4
woo.choi <woo.choi@musinsa.com>
2023-08-03 12:05:45 +0900

    feat: 해롯 데이터브릭스 DataSource 추가

---
+++ common/src/main/kotlin/com/musinsa/common/databricks/config/DatabricksDataSourceConfig.kt
@@ -0,0 +1,30 @@
+package com.musinsa.common.databricks.config
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
+
+abstract class DatabricksDataSourceConfig {
+    /**
+     * JDBC 기본 드라이버
+     */
+    @Suppress("PrivatePropertyName")
+    private val DRIVER_CLASS_NAME = "com.databricks.client.jdbc.Driver"
+
+    @Suppress("PropertyName")
+    val TIMEOUT = 60 * 5 // 타임아웃 5분
+
+    /**
+     * 데이터브릭스 클러스터 주소
+     */
+    @Suppress("PropertyName")
+    abstract val URL: String
+
+    /**
+     * databricks connection config Bean 생성
+     */
+    fun databricksDataSourceProperties(): DataSourceProperties {
+        val dataSourceProperties = DataSourceProperties()
+        dataSourceProperties.driverClassName = DRIVER_CLASS_NAME
+        dataSourceProperties.url = URL
+        return dataSourceProperties
+    }
+}
