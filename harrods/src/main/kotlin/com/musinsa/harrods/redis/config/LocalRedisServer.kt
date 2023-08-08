b4430659098d4b7335890a499184b72535cd504e
woo.choi <woo.choi@musinsa.com>
2023-08-08 14:31:21 +0900

    docs: 로컬 Redis 추가 이유 주석 추가

--- harrods/src/main/kotlin/com/musinsa/harrods/redis/config/LocalRedisServer.kt
+++ harrods/src/main/kotlin/com/musinsa/harrods/redis/config/LocalRedisServer.kt
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import redis.embedded.RedisServer
 
+/**
+ * 로컬, 테스트 환경에서 서비스 실행을 위해 Redis 클러스터 구현
+ */
 @Profile(value = ["local", "test"])
 @Configuration
 class LocalRedisServer(
