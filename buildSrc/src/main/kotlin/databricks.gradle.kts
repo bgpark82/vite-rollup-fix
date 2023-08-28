e4d055a9f72d41774244dd703067ed562545f189
woo.choi <woo.choi@musinsa.com>
2023-08-28 18:27:56 +0900

    docs: 버그 관련 TODO 추가

--- buildSrc/src/main/kotlin/databricks.gradle.kts
+++ buildSrc/src/main/kotlin/databricks.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     // Databricks JDBC connect
     implementation("com.databricks:databricks-jdbc:2.6.25-1")
 
+    // TODO JPA 의존성 분리 고민. 이것 때문에 JPA사용하지 않는 어플리케이션이 Boot가 불가능함.
     // JPA
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
