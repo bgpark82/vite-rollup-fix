7ed969591382807682df29bc17ae50e14eefe62c
woo.choi <woo.choi@musinsa.com>
2023-08-29 17:43:13 +0900

    test: 테스트용 임시 로직 추가

--- harrods-client/src/main/kotlin/com/musinsa/harrodsclient/redis/service/RedisClient.kt
+++ harrods-client/src/main/kotlin/com/musinsa/harrodsclient/redis/service/RedisClient.kt
@@ -4,5 +4,12 @@ import io.lettuce.core.api.sync.RedisCommands
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 
+/**
+ * Redis Client Service
+ */
 @Service
-class RedisClient(@Qualifier("redisCommands") private val redisCommands: RedisCommands<String, String>)
+class RedisClient(@Qualifier("redisCommands") private val redisCommands: RedisCommands<String, String>) {
+    fun get(key: String): String {
+        return redisCommands.get(key)
+    }
+}
--- harrods-client/src/test/kotlin/com/musinsa/harrodsclient/redis/service/RedisClientTest.kt
+++ harrods-client/src/test/kotlin/com/musinsa/harrodsclient/redis/service/RedisClientTest.kt
@@ -1,5 +1,38 @@
 package com.musinsa.harrodsclient.redis.service
 
-import org.junit.jupiter.api.Assertions.*
+import io.lettuce.core.api.sync.RedisCommands
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 
-internal class RedisClientTest
+/**
+ * Redis Client Service Test
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+internal class RedisClientTest {
+    @Autowired
+    lateinit var redisClient: RedisClient
+
+    @Autowired
+    lateinit var redisCommands: RedisCommands<String, String>
+
+    @BeforeEach
+    fun setUp() {
+        // 내장 Redis Clear all
+        redisCommands.flushall()
+    }
+
+    @Test
+    fun 아이템을_가져온다() {
+        // given
+        redisCommands.set("test-key", "value")
+
+        // when
+        val test = redisClient.get("test-key")
+
+        // then
+        assertThat(test).isEqualTo("value")
+    }
+}
