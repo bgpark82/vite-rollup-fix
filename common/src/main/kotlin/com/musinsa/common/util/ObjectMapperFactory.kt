3de11aa8f3df9fd4346338249bd5d5d91b00bec3
woo.choi <woo.choi@musinsa.com>
2023-08-30 16:57:38 +0900

    feat: List<Map<>> 직렬화 추가

--- common/src/main/kotlin/com/musinsa/common/util/ObjectMapperFactory.kt
+++ common/src/main/kotlin/com/musinsa/common/util/ObjectMapperFactory.kt
@@ -1,5 +1,6 @@
 package com.musinsa.common.util
 
+import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.musinsa.common.error.CommonError
 
@@ -39,6 +40,25 @@ object ObjectMapperFactory {
     ): List<T> {
         return try {
             mapper.readValue(jsonString, valueType).toList()
+        } catch (e: Exception) {
+            CommonError.FAIL_CONVERT_STRING_TO_JSON.throwMe()
+        }
+    }
+
+    /**
+     * String to JSON List 변환
+     *
+     * @param jsonString 문자열
+     * @param typeRef TypeReference
+     *
+     * @return List 객체
+     */
+    fun <T> readValues(
+        jsonString: String,
+        typeRef: TypeReference<List<T>>
+    ): List<T> {
+        return try {
+            mapper.readValue(jsonString, typeRef)
         } catch (e: Exception) {
             CommonError.FAIL_CONVERT_STRING_TO_JSON.throwMe()
         }
--- harrods-client/src/main/kotlin/com/musinsa/harrodsclient/redis/service/RedisClient.kt
+++ harrods-client/src/main/kotlin/com/musinsa/harrodsclient/redis/service/RedisClient.kt
@@ -1,5 +1,7 @@
 package com.musinsa.harrodsclient.redis.service
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.musinsa.common.util.ObjectMapperFactory
 import io.lettuce.core.api.sync.RedisCommands
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
@@ -9,7 +11,20 @@ import org.springframework.stereotype.Service
  */
 @Service
 class RedisClient(@Qualifier("redisCommands") private val redisCommands: RedisCommands<String, String>) {
-    fun get(key: String): String {
-        return redisCommands.get(key)
+    
+    /**
+     * 캐시에 저장된 값을 가져온다.
+     *
+     * @param key 캐시 키
+     *
+     * @return 캐시 값
+     * @throws
+     */
+    fun get(key: String): List<Map<String, Any>> {
+        return ObjectMapperFactory.readValues(
+            // TODO Empty key-value 에 대한 예외처리 추가
+            redisCommands.get(key),
+            object : TypeReference<List<Map<String, Any>>>() {}
+        )
     }
 }
