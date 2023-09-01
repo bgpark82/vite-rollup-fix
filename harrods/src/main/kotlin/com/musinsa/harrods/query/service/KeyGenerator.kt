49a54fc784007cc4339c9829d4ec3f52fc6413e8
musinsa-bgpark <peter.park@musinsa.com>
2023-09-01 18:58:48 +0900

    doc: KeyGenerator 문서 추가

--- harrods/src/main/kotlin/com/musinsa/harrods/query/service/KeyGenerator.kt
+++ harrods/src/main/kotlin/com/musinsa/harrods/query/service/KeyGenerator.kt
@@ -10,10 +10,13 @@ class KeyGenerator {
 
     /**
      * 쿼리와 파라미터의 조합으로 캐시 키를 생성한다
-     * harrods:1802293498:name:peter:age:30
+     *
+     * [service name]:[user Id]:[query hash]:[key:value]
+     * harrods:peter.park:1802293498:name:peter:age:30
      *
      * @param query 생성된 쿼리
      * @param params 파라미터 조합
+     * @param userId 등록자 아이디
      * @return 생성된 키
      */
     fun generate(query: String, params: Map<String, Any>, userId: String): String {
