b5576ddc0b55239b094dec78fd1016008ffd3a3c
woo.choi <woo.choi@musinsa.com>
2023-09-01 14:11:28 +0900

    feat: 모든_키_아이템을_가져온다

--- harrods-client/src/main/kotlin/com/musinsa/harrodsclient/redis/service/RedisClient.kt
+++ harrods-client/src/main/kotlin/com/musinsa/harrodsclient/redis/service/RedisClient.kt
@@ -1,7 +1,7 @@
 package com.musinsa.harrodsclient.redis.service
 
-import com.fasterxml.jackson.core.type.TypeReference
 import com.musinsa.common.util.ObjectMapperFactory
+import com.musinsa.common.util.ObjectMapperFactory.typeRefListMapAny
 import io.lettuce.core.api.sync.RedisCommands
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service
  */
 @Service
 class RedisClient(@Qualifier("redisCommands") private val redisCommands: RedisCommands<String, String>) {
-
     /**
      * 캐시에 저장된 값을 가져온다.
      *
@@ -23,22 +22,26 @@ class RedisClient(@Qualifier("redisCommands") private val redisCommands: RedisCo
         return ObjectMapperFactory.readValues(
             // TODO Empty key-value 에 대해서는 Empty 값 리턴하도록 수정
             redisCommands.get(key),
-            object : TypeReference<List<Map<String, Any>>>() {}
+            typeRefListMapAny
         )
     }
 
     /**
      * 입력된 캐시 키와 맞는 캐시에 저장된 값을 모두 가져온다.
      *
-     * @return 캐시 키 List
+     * @param keys 캐시 키 List
      *
      * @return 모든 캐시값
      */
     // TODO key 갯수 제한. 1000개
     fun getAll(keys: Array<String>): List<Map<String, Any>> {
-        var results: MutableList<Map<String, Any>> = mutableListOf()
-//        redisCommands.mget(keys)
-        return results
+        return redisCommands.mget(*keys).map { keyValue ->
+            mapOf(
+                keyValue.key to ObjectMapperFactory.readValues(
+                    keyValue.value,
+                    typeRefListMapAny
+                )
+            )
+        }.toList()
     }
-
 }
--- harrods-client/src/test/kotlin/com/musinsa/harrodsclient/redis/service/RedisClientTest.kt
+++ harrods-client/src/test/kotlin/com/musinsa/harrodsclient/redis/service/RedisClientTest.kt
@@ -1,6 +1,7 @@
 package com.musinsa.harrodsclient.redis.service
 
 import com.musinsa.common.util.ObjectMapperFactory
+import com.musinsa.common.util.ObjectMapperFactory.typeRefListMapAny
 import io.lettuce.core.api.sync.RedisCommands
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
@@ -23,6 +24,10 @@ internal class RedisClientTest {
     private val 준비코드_BOOK_VALUE = """
             [{"title":"CPP","author":"Milton","year":"2008","price":"456.00"},{"title":"JAVA","author":"Gilson","year":"2002","price":"456.00"}]
     """.trimIndent()
+    private val 준비코드_GLOSSARY_KEY = "glossary"
+    private val 준비코드_GLOSSARY_VALUE = """
+        [{"title":"example glossary","GlossDiv":{"title":"S","GlossList":{"GlossEntry":{"ID":"SGML","SortAs":"SGML","GlossTerm":"Standard Generalized Markup Language","Acronym":"SGML","Abbrev":"ISO 8879:1986","GlossDef":{"para":"A meta-markup language, used to create markup languages such as DocBook.","GlossSeeAlso":["GML","XML"]},"GlossSee":"markup"}}}}]
+    """.trimIndent()
 
     @BeforeEach
     fun setUp() {
@@ -43,6 +48,31 @@ internal class RedisClientTest {
 
     @Test
     fun 모든_키_아이템을_가져온다() {
+        redisCommands.set(준비코드_BOOK_KEY, 준비코드_BOOK_VALUE)
+        redisCommands.set(준비코드_GLOSSARY_KEY, 준비코드_GLOSSARY_VALUE)
+
+        val 결과값 = sut.getAll(arrayOf(준비코드_BOOK_KEY, 준비코드_GLOSSARY_KEY))
+
+        assertThat(결과값).isEqualTo(
+            listOf(
+                mapOf(
+                    준비코드_BOOK_KEY to ObjectMapperFactory.readValues(
+                        준비코드_BOOK_VALUE,
+                        typeRefListMapAny
+                    )
+                ),
+                mapOf(
+                    준비코드_GLOSSARY_KEY to ObjectMapperFactory.readValues(
+                        준비코드_GLOSSARY_VALUE,
+                        typeRefListMapAny
+                    )
+                )
+            )
+        )
+    }
+
+    @Test
+    fun 존재하지_않는_키에_대해서는_빈값을_리스트로_가져온다() {
         // given
 
         // when
