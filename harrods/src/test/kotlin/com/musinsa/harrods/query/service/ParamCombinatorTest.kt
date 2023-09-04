1b6bbdd3c4eb3c6581628607cc3e4d6b9d064775
musinsa-bgpark <peter.park@musinsa.com>
2023-09-04 17:52:35 +0900

    refactor: 객체 추출 및 테스트 파라미터 이름 한글화

--- harrods/src/test/kotlin/com/musinsa/harrods/query/service/ParamCombinatorTest.kt
+++ harrods/src/test/kotlin/com/musinsa/harrods/query/service/ParamCombinatorTest.kt
@@ -8,15 +8,16 @@ import org.junit.jupiter.api.assertThrows
 
 class ParamCombinatorTest {
 
+    private val combinator = ParamCombinator()
+
     @Test
     fun `파라미터의 조합을 생성한다`() {
-        val combinator = ParamCombinator()
-        val params = mapOf(
+        val 문자와_숫자_타입_파라미터 = mapOf(
             "name" to listOf("peter", "woo"),
             "age" to listOf(10, 20)
         )
 
-        val result = combinator.generate(params)
+        val result = combinator.generate(문자와_숫자_타입_파라미터)
 
         assertThat(result).containsExactlyInAnyOrder(
             mapOf("name" to "peter", "age" to 10),
@@ -28,13 +29,12 @@ class ParamCombinatorTest {
 
     @Test
     fun `리스트 파라미터를 포함한다`() {
-        val combinator = ParamCombinator()
-        val params = mapOf(
+        val 리스트_타입을_포함한_파라미터 = mapOf(
             "name" to listOf("peter", "woo"),
             "age" to listOf(listOf(10, 20), 30)
         )
 
-        val result = combinator.generate(params)
+        val result = combinator.generate(리스트_타입을_포함한_파라미터)
 
         assertThat(result).containsExactlyInAnyOrder(
             mapOf("name" to "peter", "age" to listOf(10, 20)),
@@ -45,28 +45,7 @@ class ParamCombinatorTest {
     }
 
     @Test
-    fun `파라미터가 없으면 빈 조합을 생성한다`() {
-        val combinator = ParamCombinator()
-        val params = null
-
-        val result = combinator.generate(params)
-
-        assertThat(result).containsExactly(mapOf())
-    }
-
-    @Test
-    fun `파라미터가 비면 빈 조합을 생성한다`() {
-        val combinator = ParamCombinator()
-        val params = mapOf<String, Any>()
-
-        val result = combinator.generate(params)
-
-        assertThat(result).containsExactly(mapOf())
-    }
-
-    @Test
     fun `지원하지 않는 파라미터 타입은 에러를 발생한다`() {
-        val combinator = ParamCombinator()
         val 지원하지_않는_파라미터_타입 = mapOf<String, Any>("name" to mapOf("hello" to "world"))
 
         val result = assertThrows<CodeAwareException> {
@@ -75,5 +54,23 @@ class ParamCombinatorTest {
 
         assertThat(result.message).isEqualTo("지원하지 않는 파라미터 타입")
         assertThat(result.error).isEqualTo(ErrorCode.UNSUPPORTED_PARAMETER_TYPE)
+    }
+
+    @Test
+    fun `파라미터가 없으면 빈 조합을 생성한다`() {
+        val null_파라미터 = null
+
+        val result = combinator.generate(null_파라미터)
+
+        assertThat(result).containsExactly(mapOf())
+    }
+
+    @Test
+    fun `파라미터가 비면 빈 조합을 생성한다`() {
+        val 빈_파라미터 = mapOf<String, Any>()
+
+        val result = combinator.generate(빈_파라미터)
+
+        assertThat(result).containsExactly(mapOf())
     }
 }
