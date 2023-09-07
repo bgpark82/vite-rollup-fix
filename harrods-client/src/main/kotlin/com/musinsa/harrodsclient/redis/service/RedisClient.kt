8977cc70676b793e16efeb65bdcbb13e13200f8f
woo.choi <woo.choi@musinsa.com>
2023-09-07 18:22:42 +0900

    fix: RedisStringCommands 주입방식으로 변경 - dev, prod 환경에서는 RedisAdvancedClusterCommands 사용 - local, test 환경에서는 RedisCommands 사용 RedisAdvancedClusterCommands가 RedisCommands를 상속받지 않고 있어서 동일한 자료형으로 주입을 받을수 없어서 둘의 공통 상위 인터페이스인 RedisStringCommands 자료형 주입으로 수정

--- harrods-client/src/main/kotlin/com/musinsa/harrodsclient/redis/service/RedisClient.kt
+++ harrods-client/src/main/kotlin/com/musinsa/harrodsclient/redis/service/RedisClient.kt
@@ -3,7 +3,7 @@ package com.musinsa.harrodsclient.redis.service
 import com.musinsa.common.util.ObjectMapperFactory
 import com.musinsa.common.util.ObjectMapperFactory.typeRefListMapAny
 import com.musinsa.harrodsclient.redis.dto.Search
-import io.lettuce.core.api.sync.RedisCommands
+import io.lettuce.core.api.sync.RedisStringCommands
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 
@@ -11,7 +11,12 @@ import org.springframework.stereotype.Service
  * Redis Client Service
  */
 @Service
-class RedisClient(@Qualifier("redisCommands") private val redisCommands: RedisCommands<String, String>) {
+class RedisClient(
+    /**
+     * 키 조회만 하는 역할이기 때문에, RedisStringCommands 형태로 주입받는다.
+     */
+    @Qualifier("redisCommands") private val redisStringCommands: RedisStringCommands<String, String>
+) {
     /**
      * 입력된 캐시 키와 맞는 캐시에 저장된 값을 모두 가져온다.
      *
@@ -20,7 +25,7 @@ class RedisClient(@Qualifier("redisCommands") private val redisCommands: RedisCo
      * @return 모든 캐시값
      */
     fun getAll(search: Search): List<Map<String, Any>> {
-        return redisCommands.mget(*search.keys).map { keyValue ->
+        return redisStringCommands.mget(*search.keys).map { keyValue ->
             when (keyValue.hasValue()) {
                 true -> mapOf(
                     keyValue.key to ObjectMapperFactory.readValues(
