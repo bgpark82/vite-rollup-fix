7ea6a999193fc148cff3eceac42359168d6f18c4
woo.choi <woo.choi@musinsa.com>
2023-10-23 17:43:12 +0900

    test: 아스키독 생성 테스트 추가

--- harrods-client/src/main/kotlin/com/musinsa/harrodsclient/redis/controller/RedisController.kt
+++ harrods-client/src/main/kotlin/com/musinsa/harrodsclient/redis/controller/RedisController.kt
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-// TODO Rest Docs 생성
 @RestController
 @RequestMapping("/cache")
 class RedisController(private val redisClient: RedisClient) {
