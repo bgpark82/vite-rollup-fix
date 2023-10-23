ce5d508e4198424c21fce91b33db046939d04b54
woo.choi <woo.choi@musinsa.com>
2023-10-23 15:27:24 +0900

    feat: 코루틴 suspend 테스트 검증 메소드 추가

--- common/src/testFixtures/kotlin/com/musinsa/common/restdoc/RestDocsHelper.kt
+++ common/src/testFixtures/kotlin/com/musinsa/common/restdoc/RestDocsHelper.kt
@@ -1,6 +1,7 @@
 package com.musinsa.common.restdoc
 
 import com.musinsa.common.error.CommonError
+import org.junit.jupiter.params.shadow.com.univocity.parsers.conversions.Conversions.string
 import org.springframework.http.MediaType
 import org.springframework.restdocs.RestDocumentationContextProvider
 import org.springframework.restdocs.http.HttpDocumentation
@@ -25,6 +26,7 @@ import org.springframework.test.web.servlet.ResultActions
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.request
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.util.MultiValueMap
@@ -183,6 +185,17 @@ fun MockMvc.POST(
 fun ResultActions.성공_검증(예상_응답: String): ResultActions {
     return this.andExpect(MockMvcResultMatchers.status().isOk)
         .andExpect(content().string(예상_응답))
+}
+
+/**
+ * Coroutine suspend 메소드 테스트용도
+ *
+ * @see https://docs.spring.io/spring-framework/reference/testing/spring-mvc-test-framework/async-requests.html
+ */
+fun ResultActions.성공_검증_AWAIT(예상_응답: String): ResultActions {
+    return this.andExpect(MockMvcResultMatchers.status().isOk)
+        .andExpect(request().asyncStarted())
+        .andExpect(request().asyncResult(예상_응답))
 }
 
 /**
