70220c85cdf9994770509011443bae9ec8d33149
woo.choi <woo.choi@musinsa.com>
2023-10-25 15:34:27 +0900

    feat: 품목 추가

---
+++ stat/src/main/kotlin/com/musinsa/stat/sales/domain/GoodsKind.kt
@@ -0,0 +1,19 @@
+package com.musinsa.stat.sales.domain
+
+/**
+ * 품목 (상품 옵션)
+ */
+enum class GoodsKind(val description: String) {
+    CLOTHES("CLOTHES"),
+    BEAUTY("BEAUTY"),
+    TICKET("TICKET"),
+    SHOES("SHOES"),
+    HAT("HAT"),
+    BAG("BAG"),
+    WATCH("WATCH"),
+    CAP("CAP"),
+    ETC("ETC"),
+    ACCESSORY("ACCESSORY"),
+    Watch("Watch"),
+    WEBBING("Webbing : Acrylic")
+}
