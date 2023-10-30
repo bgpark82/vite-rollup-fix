3987509fab3a2e5d30b77b56e7d8e83a006e07f7
woo.choi <woo.choi@musinsa.com>
2023-10-30 13:00:02 +0900

    docs: 판매 경로 설명 수정

--- stat/src/main/kotlin/com/musinsa/stat/sales/domain/SalesFunnel.kt
+++ stat/src/main/kotlin/com/musinsa/stat/sales/domain/SalesFunnel.kt
@@ -4,7 +4,7 @@ package com.musinsa.stat.sales.domain
  * 판매 경로
  */
 enum class SalesFunnel(val description: String) {
-    DEFAULT("기본값"),
+    DEFAULT("기본값. 선택 없음"),
     MOBILE_WEB("모바일 웹"),
     MOBILE_APP("모바일 앱")
 }
