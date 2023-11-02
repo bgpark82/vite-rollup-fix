354328865ef1ad9d407c1995d14b21c0a61a2ce2
woo.choi <woo.choi@musinsa.com>
2023-11-02 14:33:42 +0900

    feat: 모바일 웹+앱 케이스 로직 추가

--- stat/src/main/kotlin/com/musinsa/stat/sales/domain/SalesFunnel.kt
+++ stat/src/main/kotlin/com/musinsa/stat/sales/domain/SalesFunnel.kt
@@ -7,5 +7,5 @@ enum class SalesFunnel(val description: String) {
     DEFAULT("기본값. 선택 없음(defaultValue)"),
     MOBILE_WEB("모바일 웹"),
     MOBILE_APP("모바일 앱"),
-    MOBILE_ALL("모바일 웹+앱"),
+    MOBILE_ALL("모바일 웹+앱")
 }
