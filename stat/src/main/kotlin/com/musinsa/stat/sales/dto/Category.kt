d8cf9d70cb8fa1fb0be2d8321b9240a008615db3
woo.choi <woo.choi@musinsa.com>
2023-11-03 15:41:11 +0900

    refactor: 카테고리별 매출통계 응답 DTO 수정

--- stat/src/main/kotlin/com/musinsa/stat/sales/dto/Category.kt
+++ stat/src/main/kotlin/com/musinsa/stat/sales/dto/Category.kt
@@ -3,6 +3,9 @@ package com.musinsa.stat.sales.dto
 import com.fasterxml.jackson.annotation.JsonIgnore
 import java.sql.ResultSet
 
+/**
+ * 카테고리 매출통계 응답값 DTO
+ */
 data class Category(
     @JsonIgnore
     val rs: ResultSet,
@@ -23,7 +26,17 @@ data class Category(
     val smallCategoryCode: String?,
 
     /**
-     * 카테고리명
+     * 대분류 카테고리명
      */
-    val category: String?
+    val largeCategory: String?,
+
+    /**
+     * 중분류 카테고리명
+     */
+    val mediumCategory: String?,
+
+    /**
+     * 소분류 카테고리명
+     */
+    val smallCategory: String?
 ) : SalesStatisticsMetric(rs)
