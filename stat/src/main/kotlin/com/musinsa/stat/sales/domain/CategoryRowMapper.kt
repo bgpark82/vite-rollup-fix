d34c6fb0e136bfc4fc7e1441cb13e424ad03b4fe
woo.choi <woo.choi@musinsa.com>
2023-11-03 15:50:52 +0900

    refactor: Category RowMapper 수정

--- stat/src/main/kotlin/com/musinsa/stat/sales/domain/CategoryRowMapper.kt
+++ stat/src/main/kotlin/com/musinsa/stat/sales/domain/CategoryRowMapper.kt
@@ -4,15 +4,18 @@ import com.musinsa.stat.sales.dto.Category
 import org.springframework.jdbc.core.RowMapper
 import java.sql.ResultSet
 
+const val NULL_VALUE = "-"
+
 object CategoryRowMapper : RowMapper<Category> {
-    private const val NULL_VALUE = "값없음"
     override fun mapRow(rs: ResultSet, rowNum: Int): Category {
         return Category(
             rs,
             largeCategoryCode = rs.getString("대분류") ?: NULL_VALUE,
             mediumCategoryCode = rs.getString("중분류") ?: NULL_VALUE,
             smallCategoryCode = rs.getString("소분류") ?: NULL_VALUE,
-            category = rs.getString("카테고리명") ?: NULL_VALUE
+            largeCategory = rs.getString("대분류_카테고리명") ?: NULL_VALUE,
+            mediumCategory = rs.getString("중분류_카테고리명") ?: NULL_VALUE,
+            smallCategory = rs.getString("소분류_카테고리명") ?: NULL_VALUE
         )
     }
 }
