48688c84d5440dd9dde439ed10615985ade353be
woo.choi <woo.choi@musinsa.com>
2023-11-06 15:15:37 +0900

    test: 합계 구하는 테스트 시나리오 작성

--- stat/src/test/kotlin/com/musinsa/stat/sales/dto/CategorySalesStatisticsResponseTest.kt
+++ stat/src/test/kotlin/com/musinsa/stat/sales/dto/CategorySalesStatisticsResponseTest.kt
@@ -7,7 +7,6 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class CategorySalesStatisticsResponseTest {
-
     @Test
     fun `대분류 값이 없는 경우 결과값 Row 에서 삭제한다`() {
         val 원본_카테고리_리스트 = 쿼리_결과_카테고리_리스트()
@@ -20,5 +19,17 @@ internal class CategorySalesStatisticsResponseTest {
         )
 
         assertThat(정제된_응답값.content.size).isEqualTo(14)
+    }
+
+    @Test
+    fun `소분류 카테고리명이 Null 인 경우를 제외하고 합계를 구한다`() {
+        val 카테고리_리스트 = CategorySalesStatisticsResponse(
+            쿼리_결과_카테고리_리스트(),
+            PAGE_SIZE_DEFAULT_VALUE.toLong(),
+            PAGE_DEFAULT_VALUE.toLong(),
+            "originSql"
+        )
+
+        assertThat(카테고리_리스트.sum.sellQuantity).isEqualTo(280933)
     }
 }
