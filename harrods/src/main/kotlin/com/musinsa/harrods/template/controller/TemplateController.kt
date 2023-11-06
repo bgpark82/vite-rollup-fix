fda1387bd0411ac49445ca0f37893e0a66687cd9
musinsa-bgpark <peter.park@musinsa.com>
2023-11-06 14:36:47 +0900

    feat: 템플릿 조회 API 추가

--- harrods/src/main/kotlin/com/musinsa/harrods/template/controller/TemplateController.kt
+++ harrods/src/main/kotlin/com/musinsa/harrods/template/controller/TemplateController.kt
@@ -4,6 +4,7 @@ import com.musinsa.harrods.template.dto.TemplateResponse
 import com.musinsa.harrods.template.service.TemplateService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -15,5 +16,11 @@ class TemplateController(
     fun findAll(): ResponseEntity<List<TemplateResponse>> {
         val templates = templateService.findAll()
         return ResponseEntity.ok(templates.map(TemplateResponse::of))
+    }
+
+    @GetMapping("/templates/{id}")
+    fun findById(@PathVariable id: Long): ResponseEntity<TemplateResponse> {
+        val template = templateService.findById(id)
+        return ResponseEntity.ok(TemplateResponse.of(template))
     }
 }
--- harrods/src/main/kotlin/com/musinsa/harrods/template/service/TemplateService.kt
+++ harrods/src/main/kotlin/com/musinsa/harrods/template/service/TemplateService.kt
@@ -1,7 +1,9 @@
 package com.musinsa.harrods.template.service
 
+import com.musinsa.harrods.error.ErrorCode
 import com.musinsa.harrods.template.domain.Template
 import com.musinsa.harrods.template.domain.TemplateRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -13,5 +15,10 @@ class TemplateService(
     @Transactional(readOnly = true)
     fun findAll(): List<Template> {
         return templateRepository.findAll()
+    }
+
+    @Transactional(readOnly = true)
+    fun findById(id: Long): Template {
+        return templateRepository.findByIdOrNull(id) ?: ErrorCode.QUERY_NOT_FOUND.throwMe()
     }
 }
