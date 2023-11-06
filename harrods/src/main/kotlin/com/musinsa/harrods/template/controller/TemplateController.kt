95444bd2fbfb13b4f3e4c70e91c06a74f407c9c8
musinsa-bgpark <peter.park@musinsa.com>
2023-11-06 14:38:09 +0900

    refactor: 기본 매핑 경로 통합

--- harrods/src/main/kotlin/com/musinsa/harrods/template/controller/TemplateController.kt
+++ harrods/src/main/kotlin/com/musinsa/harrods/template/controller/TemplateController.kt
@@ -5,20 +5,22 @@ import com.musinsa.harrods.template.service.TemplateService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/templates")
 class TemplateController(
     private val templateService: TemplateService
 ) {
 
-    @GetMapping("/templates")
+    @GetMapping
     fun findAll(): ResponseEntity<List<TemplateResponse>> {
         val templates = templateService.findAll()
         return ResponseEntity.ok(templates.map(TemplateResponse::of))
     }
 
-    @GetMapping("/templates/{id}")
+    @GetMapping("/{id}")
     fun findById(@PathVariable id: Long): ResponseEntity<TemplateResponse> {
         val template = templateService.findById(id)
         return ResponseEntity.ok(TemplateResponse.of(template))
