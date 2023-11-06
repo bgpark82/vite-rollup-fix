c12b0e4e4a2dd126440afba11e3b63e9d6eef243
woo.choi <woo.choi@musinsa.com>
2023-11-06 14:39:20 +0900

    feat: 카테고리 결과값에서 대분류 값이 없는 row 제거

--- stat/src/main/kotlin/com/musinsa/stat/sales/dto/CategorySalesStatisticsResponse.kt
+++ stat/src/main/kotlin/com/musinsa/stat/sales/dto/CategorySalesStatisticsResponse.kt
@@ -9,11 +9,18 @@ import com.musinsa.stat.sales.error.SalesError
  * @see <a href="https://wiki.musinsa.com/pages/viewpage.action?pageId=169800267">카테고리별 매출통계 특이성</a>
  */
 class CategorySalesStatisticsResponse(
-    jdbcQueryResult: List<SalesStatisticsMetric>,
+    jdbcQueryResult: List<Category>,
+
+    // 페이지 사이즈
     val pageSize: Long,
+
+    // 페이지
     val page: Long,
     originSql: String
 ) {
+
+    // 결과값
+    val content: List<Category>
 
     init {
         // 검색 결과 없는 경우
@@ -21,7 +28,9 @@ class CategorySalesStatisticsResponse(
             throw SalesError.NON_EXIST_SALES_STATISTICS_DATA.throwMe()
         }
 
-        // 대분류 값이 없는 경우 결과값 Row 에서 삭제
-        // TODO
+        // 대분류 값이 없는 경우는 불필요한 쿼리 결과이므로, 결과값 Row 에서 삭제
+        content = jdbcQueryResult.filter {
+            !it.largeCategoryCode.isNullOrBlank()
+        }
     }
 }
