c4b4689e018b15d74929489cf591aa819dfb4f9b
musinsa-bgpark <peter.park@musinsa.com>
2023-11-06 17:10:09 +0900

    refactor: 템플릿 생성 로직에서 쿼리 생성 로직 분리

--- harrods/src/main/kotlin/com/musinsa/harrods/query/service/QueryService.kt
+++ harrods/src/main/kotlin/com/musinsa/harrods/query/service/QueryService.kt
@@ -3,14 +3,53 @@ package com.musinsa.harrods.query.service
 import com.musinsa.harrods.error.ErrorCode
 import com.musinsa.harrods.query.domain.Query
 import com.musinsa.harrods.query.domain.QueryRepository
+import com.musinsa.harrods.template.dto.TemplateRequest
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class QueryService(
-    private val queryRepository: QueryRepository,
+    private val paramCombinator: ParamCombinator,
+    private val keyGenerator: KeyGenerator,
+    private val queryGenerator: QueryGenerator,
+    private val queryRepository: QueryRepository
 ) {
+
+    /**
+     * 템플릿과 파라미터로 쿼리를 생성한다
+     *
+     * @param template 검증된 템플릿
+     * @param request 요청 파라미터
+     *
+     * @return 생성된 쿼리 리스트
+     */
+    @Transactional
+    fun generate(
+        template: String,
+        request: TemplateRequest
+    ): MutableList<Query> {
+        val queries = mutableListOf<Query>()
+
+        for (param in paramCombinator.generate(request.params)) {
+            val query = queryGenerator.generate(template, param)
+
+            queries.add(
+                Query.create(
+                    ttl = request.ttl,
+                    queries = query,
+                    cacheKey = keyGenerator.generate(query, request.userId),
+                    scheduleInterval = request.interval,
+                    userId = request.userId,
+                    cacheKeySuffix = request.alias
+                )
+            )
+        }
+
+        validateKeyExist(queries)
+        return queries
+    }
+
     @Transactional(readOnly = true)
     fun findAll(): List<Query> {
         return queryRepository.findAll()
@@ -19,5 +58,16 @@ class QueryService(
     @Transactional(readOnly = true)
     fun findById(id: Long): Query {
         return queryRepository.findByIdOrNull(id) ?: ErrorCode.QUERY_NOT_FOUND.throwMe()
+    }
+
+    /**
+     * 기존에 등록된 쿼리 여부 확인
+     */
+    private fun validateKeyExist(queries: List<Query>) {
+        val keys = queries.map(Query::cacheKey)
+
+        if (queryRepository.existsByCacheKeyIn(keys)) {
+            return ErrorCode.QUERY_ALREADY_EXIST.throwMe()
+        }
     }
 }
--- harrods/src/main/kotlin/com/musinsa/harrods/template/service/TemplateService.kt
+++ harrods/src/main/kotlin/com/musinsa/harrods/template/service/TemplateService.kt
@@ -1,11 +1,7 @@
 package com.musinsa.harrods.template.service
 
 import com.musinsa.harrods.error.ErrorCode
-import com.musinsa.harrods.query.domain.Query
-import com.musinsa.harrods.query.domain.QueryRepository
-import com.musinsa.harrods.query.service.KeyGenerator
-import com.musinsa.harrods.query.service.ParamCombinator
-import com.musinsa.harrods.query.service.QueryGenerator
+import com.musinsa.harrods.query.service.QueryService
 import com.musinsa.harrods.query.service.TemplateFormatter
 import com.musinsa.harrods.template.domain.Template
 import com.musinsa.harrods.template.domain.TemplateRepository
@@ -16,12 +12,9 @@ import org.springframework.transaction.annotation.Transactional
 
 @Service
 class TemplateService(
-    private val paramCombinator: ParamCombinator,
-    private val keyGenerator: KeyGenerator,
-    private val queryGenerator: QueryGenerator,
-    private val queryRepository: QueryRepository,
     private val templateFormatter: TemplateFormatter,
-    private val templateRepository: TemplateRepository
+    private val templateRepository: TemplateRepository,
+    private val queryService: QueryService
 ) {
 
     /**
@@ -32,32 +25,13 @@ class TemplateService(
      */
     @Transactional
     fun create(request: TemplateRequest): Template {
-        val (template, params, ttl, interval, userId, alias) = request
-        val queries = mutableListOf<Query>()
-
-        val formattedTemplate = templateFormatter.format(template)
-
-        for (param in paramCombinator.generate(params)) {
-            val query = queryGenerator.generate(formattedTemplate, param)
-
-            queries.add(
-                Query.create(
-                    ttl = ttl,
-                    queries = query,
-                    cacheKey = keyGenerator.generate(query, userId),
-                    scheduleInterval = interval,
-                    userId = userId,
-                    cacheKeySuffix = alias
-                )
-            )
-        }
-
-        validateKeyExist(queries)
+        val formattedTemplate = templateFormatter.format(request.template)
+        val queries = queryService.generate(formattedTemplate, request)
 
         return templateRepository.save(
             Template.create(
                 name = "브랜드별 통계",
-                userId = userId,
+                userId = request.userId,
                 queries = queries
             )
         )
@@ -71,16 +45,5 @@ class TemplateService(
     @Transactional(readOnly = true)
     fun findById(id: Long): Template {
         return templateRepository.findByIdOrNull(id) ?: ErrorCode.QUERY_NOT_FOUND.throwMe()
-    }
-
-    /**
-     * 기존에 등록된 쿼리 여부 확인
-     */
-    private fun validateKeyExist(queries: List<Query>) {
-        val keys = queries.map(Query::cacheKey)
-
-        if (queryRepository.existsByCacheKeyIn(keys)) {
-            return ErrorCode.QUERY_ALREADY_EXIST.throwMe()
-        }
     }
 }
