57c691c53190d43a39528e99109647e3f603cc17
woo.choi <woo.choi@musinsa.com>
2023-11-07 13:12:35 +0900

    feat: 김기범님 요청으로 헤더 추가

--- common/src/main/kotlin/com/musinsa/common/config/CorsConfig.kt
+++ common/src/main/kotlin/com/musinsa/common/config/CorsConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+//TODO harrods-client 의 경우 coroutine 사용으로 netty framework 사용중이어서, 아래 Cors 설정이 적용 안될것 같다. 확인 필요
 @Configuration
 class CorsConfig : WebMvcConfigurer {
 
@@ -17,7 +18,7 @@ class CorsConfig : WebMvcConfigurer {
                 "POST", "GET", "OPTIONS", "DELETE",
                 "PUT"
             ).allowedHeaders(
-                "Content-Type", "Accept"
+                "Content-Type", "Accept", "Access-Control-Request-Headers"
             )
     }
 }
