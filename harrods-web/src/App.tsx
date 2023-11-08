5573b781126d1e3bb3503f7da3ef8ab1fcb76415
musinsa-bgpark <peter.park@musinsa.com>
2023-11-08 15:55:57 +0900

    feat: 쿼리 조회 페이지 적용

--- harrods-web/src/App.tsx
+++ harrods-web/src/App.tsx
@@ -3,8 +3,9 @@ import {harrodsDataProvider} from "../provider/HarrodsProvider";
 import {TemplateList} from "./templates/TemplateList";
 import {TemplateShow} from "./templates/TemplateShow";
 import {QueryList} from "./queries/QueryList";
+import {QueryShow} from "./queries/QueryShow";
 
 export const App = () => <Admin dataProvider={harrodsDataProvider}>
     <Resource name="templates" list={TemplateList} show={TemplateShow} />
-    <Resource name="queries" list={QueryList}/>
+    <Resource name="queries" list={QueryList} show={QueryShow}/>
 </Admin>;
