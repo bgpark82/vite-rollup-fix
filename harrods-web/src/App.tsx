b103ddc14f50a91b4347d5bd7a050e90a8e2d773
musinsa-bgpark <peter.park@musinsa.com>
2023-11-08 15:44:03 +0900

    feat: 쿼리 리소스 추가

--- harrods-web/src/App.tsx
+++ harrods-web/src/App.tsx
@@ -1,8 +1,9 @@
-import {Admin, Resource, ShowGuesser,} from "react-admin";
+import {Admin, Resource,} from "react-admin";
 import {harrodsDataProvider} from "../provider/HarrodsProvider";
 import {TemplateList} from "./templates/TemplateList";
 import {TemplateShow} from "./templates/TemplateShow";
 
 export const App = () => <Admin dataProvider={harrodsDataProvider}>
     <Resource name="templates" list={TemplateList} show={TemplateShow} />
+    <Resource name="queries"/>
 </Admin>;
