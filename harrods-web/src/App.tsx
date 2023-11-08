2d0beea4f3571f9870a9b20b62f96402612da1db
musinsa-bgpark <peter.park@musinsa.com>
2023-11-08 15:53:10 +0900

    feat: 쿼리 리스트 조회 페이지 적용

--- harrods-web/src/App.tsx
+++ harrods-web/src/App.tsx
@@ -2,8 +2,9 @@ import {Admin, Resource,} from "react-admin";
 import {harrodsDataProvider} from "../provider/HarrodsProvider";
 import {TemplateList} from "./templates/TemplateList";
 import {TemplateShow} from "./templates/TemplateShow";
+import {QueryList} from "./queries/QueryList";
 
 export const App = () => <Admin dataProvider={harrodsDataProvider}>
     <Resource name="templates" list={TemplateList} show={TemplateShow} />
-    <Resource name="queries"/>
+    <Resource name="queries" list={QueryList}/>
 </Admin>;
