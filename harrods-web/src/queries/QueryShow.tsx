02065f1aeee547c2bfef7561e26be0b9b9293895
musinsa-bgpark <peter.park@musinsa.com>
2023-11-08 23:45:39 +0900

    chore: 레이블 추가

--- harrods-web/src/queries/QueryShow.tsx
+++ harrods-web/src/queries/QueryShow.tsx
@@ -5,15 +5,15 @@ export const QueryShow = () => {
     return (
         <Show>
             <SimpleShowLayout>
-                <QueryField source="query" width="100%" height="400px"/>
-                <TextField source="key"/>
-                <NumberField source="ttl"/>
-                <TextField source="interval"/>
-                <TextField source="userId"/>
-                <TextField source="createdDateTime"/>
-                <TextField source="modifiedDateTime"/>
-                <TextField source="lastCachedDateTime" emptyText="-"/>
-                <TextField source="alias"/>
+                <QueryField source="query" width="100%" height="400px" label="쿼리"/>
+                <TextField source="key" label="캐시 키"/>
+                <TextField source="alias" label="별칭"/>
+                <NumberField source="ttl" label="ttl"/>
+                <TextField source="interval" label="interval"/>
+                <TextField source="userId" label="등록인 아이디"/>
+                <TextField source="createdDateTime" label="생성일"/>
+                <TextField source="modifiedDateTime" label="수정일"/>
+                <TextField source="lastCachedDateTime" emptyText="-" label="마지막 캐시일"/>
             </SimpleShowLayout>
         </Show>
     )
