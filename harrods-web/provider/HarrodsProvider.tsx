675a446a2403ac337ff9839f1df582142bb37d9a
musinsa-bgpark <peter.park@musinsa.com>
2023-11-08 15:00:39 +0900

    feat: dataProvider 생성 및 template 리스트 조회
    - DataProvider: API 호출하여 데이터를 생성/수정/삭제/조회
    - 해롯 어드민 API 서버에서 데이터 조회
    - resource에 따라 다른 데이터를 조회
    - https://marmelab.com/react-admin/DataProviderWriting.html

---
+++ harrods-web/provider/HarrodsProvider.tsx
@@ -0,0 +1,14 @@
+import {DataProvider, fetchUtils} from "react-admin";
+
+const BASE_URL = "http://localhost:8080/admin"
+const httpClient = fetchUtils.fetchJson;
+
+// @ts-ignore
+// @see https://marmelab.com/react-admin/DataProviderWriting.html
+export const harrodsDataProvider: DataProvider =  {
+    getList: (resource: string) =>
+        httpClient(`${BASE_URL}/${resource}`)
+            .then(({json}) => (
+                {data : json, total: json.length}
+            )),
+};
