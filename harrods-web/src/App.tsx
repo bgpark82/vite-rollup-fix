57d80a7fe0d98c13fc183d6f281b94496ec187ff
musinsa-bgpark <peter.park@musinsa.com>
2023-11-08 15:04:46 +0900

    feat: templates 리소스 적용
    - harrodsDataProvider가 해롯 어드민 API의 /templates 경로로 생성/수정/삭제/조회 요청을 한다

--- harrods-web/src/App.tsx
+++ harrods-web/src/App.tsx
@@ -1,4 +1,6 @@
-import {Admin,} from "react-admin";
+import {Admin, Resource,} from "react-admin";
 import {harrodsDataProvider} from "../provider/HarrodsProvider";
 
-export const App = () => <Admin dataProvider={harrodsDataProvider}></Admin>;
+export const App = () => <Admin dataProvider={harrodsDataProvider}>
+    <Resource name="templates" />
+</Admin>;
