78959a9dbca33f37399e5234049a7ad60fb46088
musinsa-bgpark <peter.park@musinsa.com>
2023-11-08 15:21:44 +0900

    feat: 템플릿 조회 페이지 적용

--- harrods-web/src/App.tsx
+++ harrods-web/src/App.tsx
@@ -1,7 +1,8 @@
-import {Admin, Resource,} from "react-admin";
+import {Admin, Resource, ShowGuesser,} from "react-admin";
 import {harrodsDataProvider} from "../provider/HarrodsProvider";
 import {TemplateList} from "./templates/TemplateList";
+import {TemplateShow} from "./templates/TemplateShow";
 
 export const App = () => <Admin dataProvider={harrodsDataProvider}>
-    <Resource name="templates" list={TemplateList} />
+    <Resource name="templates" list={TemplateList} show={TemplateShow} />
 </Admin>;
