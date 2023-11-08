01d9c5196176b6d08d79da011c496d2a8b92538f
musinsa-bgpark <peter.park@musinsa.com>
2023-11-08 16:02:50 +0900

    feat: 템플릿 등록 페이지 적용

--- harrods-web/src/App.tsx
+++ harrods-web/src/App.tsx
@@ -4,8 +4,9 @@ import {TemplateList} from "./templates/TemplateList";
 import {TemplateShow} from "./templates/TemplateShow";
 import {QueryList} from "./queries/QueryList";
 import {QueryShow} from "./queries/QueryShow";
+import {TemplateCreate} from "./templates/TemplateCreate";
 
 export const App = () => <Admin dataProvider={harrodsDataProvider}>
-    <Resource name="templates" list={TemplateList} show={TemplateShow} />
+    <Resource name="templates" list={TemplateList} show={TemplateShow} create={TemplateCreate}/>
     <Resource name="queries" list={QueryList} show={QueryShow}/>
 </Admin>;
