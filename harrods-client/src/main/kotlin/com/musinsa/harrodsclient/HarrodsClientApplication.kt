d497020541ab2865fc922aa9831e46559755f2db
woo.choi <woo.choi@musinsa.com>
2023-11-14 13:23:24 +0900

    refactor: common, common-mvc 간 클래스 분리

--- harrods-client/src/main/kotlin/com/musinsa/harrodsclient/HarrodsClientApplication.kt
+++ harrods-client/src/main/kotlin/com/musinsa/harrodsclient/HarrodsClientApplication.kt
@@ -1,13 +1,13 @@
 package com.musinsa.harrodsclient
 
-import com.musinsa.common.aws.HealthCheckController
-import com.musinsa.common.config.CorsConfig
 import com.musinsa.common.error.RestControllerAdviceExceptionHandler
 import com.musinsa.common.redis.config.LocalReactiveRedisDataSourcePoolConfig
 import com.musinsa.common.redis.config.LocalRedisServer
 import com.musinsa.common.redis.config.ReactiveRedisDataSourcePoolConfig
 import com.musinsa.common.redis.service.LocalReactiveRedisConnection
 import com.musinsa.common.redis.service.ReactiveRedisConnection
+import com.musinsa.commonmvc.aws.HealthCheckController
+import com.musinsa.commonmvc.config.CorsConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
