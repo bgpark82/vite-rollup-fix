9580d6cd53d829dc87de600e51c47351f5ea760f
woo.choi <woo.choi@musinsa.com>
2023-11-15 09:54:30 +0900

    refactor: 헬스체크 응답값 수정

--- common-webflux/src/main/kotlin/com/musinsa/commonwebflux/aws/ReactiveHealthCheckController.kt
+++ common-webflux/src/main/kotlin/com/musinsa/commonwebflux/aws/ReactiveHealthCheckController.kt
@@ -1,10 +1,10 @@
 package com.musinsa.commonwebflux.aws
 
+import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.ok
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.buildAndAwait
 
 /**
  * AWS TargetGroup 헬스체크를 위한 컨트롤러
@@ -17,7 +17,7 @@ class ReactiveHealthCheckController {
      * AWS Auto Scaling Group Health check 용도
      */
     @GetMapping("/health")
-    suspend fun healthCheck(): ServerResponse {
-        return ServerResponse.ok().buildAndAwait()
+    suspend fun healthCheck(): ResponseEntity<Any> {
+        return ok().build()
     }
 }
